e05811d146f361029e59faee174ce7f6aee3ccfd
DariaClem <91901282+DariaClem@users.noreply.github.com>
2022-06-01 00:30:23 +0300

    Add files via upload

---
+++ fisier.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    let buton = document.getElementById("tooglenight");
+    buton.onclick = function () {
+        document.body.style.backgroundColor = 'black';
+    }
+}
